64d19a065586cda61591ff70e59c5b6fc32292f1
Nabendu Roy <rnabendu23@gmail.com>
2018-03-18 14:01:02 +0530

    initial code setup

---
+++ Entities/AuthenticationEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumbrellaAPI.Entities
+{
+    public class AuthenticationEntity
+    {
+        public string UserId { get; set; }
+
+        public string Pwd { get; set; }
+    }
+}
---
+++ Entities/DBResultEnity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumbrellaAPI.Entities
+{
+    public class DBResultEnity
+    {
+        public int STATUSCODE { get; set; }
+
+        public string STATUSDESC { get; set; }
+    }
+}
